139ba78338205e4e09825341673c4b62c913245d
Michael Barker <mikeb01@gmail.com>
2018-12-10 17:06:39 +1300

    Pass closure for flood fill.

--- src/advent6.rs
+++ src/advent6.rs
@@ -55,26 +55,36 @@ pub fn advent6a() -> Result<usize, Error> {
 
     for px in 0..grid.len() {
         let target = grid[px][0];
-        flood_fill(&mut grid, (px, 0), target, GridEntry::Infinity);
+
+        if target != GridEntry::Infinity {
+            flood_fill(&mut grid, (px, 0), |x| x == target, GridEntry::Infinity);
+        }
     }
 
     for px in 0..grid.len() {
         let last_y = grid[px].len() - 1;
         let target = grid[px][last_y];
 
-        flood_fill(&mut grid, (px, last_y), target, GridEntry::Infinity);
+        if target != GridEntry::Infinity {
+            flood_fill(&mut grid, (px, last_y), |x| x == target, GridEntry::Infinity);
+        }
     }
 
     for py in 0..grid[0].len() {
         let target = grid[0][py];
-        flood_fill(&mut grid, (0, py), target, GridEntry::Infinity);
+
+        if target != GridEntry::Infinity {
+            flood_fill(&mut grid, (0, py), |x| x == target, GridEntry::Infinity);
+        }
     }
 
     let x_last = grid.len() - 1;
     for py in 0..grid[x_last].len() {
         let target = grid[x_last][py];
 
-        flood_fill(&mut grid, (x_last, py), target, GridEntry::Infinity);
+        if target != GridEntry::Infinity {
+            flood_fill(&mut grid, (x_last, py), |x| x == target, GridEntry::Infinity);
+        }
     }
 
     let mut counts: Vec<usize> = vec![0; points.len()];
@@ -98,9 +108,11 @@ fn manhattan_distance(p: (usize, usize), q: (usize, usize)) -> usize {
     return x_diff + y_diff;
 }
 
-fn flood_fill(grid: &mut [&mut [GridEntry]], node: (usize, usize), target: GridEntry, replacement: GridEntry) {
+fn flood_fill<P>(grid: &mut [&mut [GridEntry]], node: (usize, usize), predicate: P, replacement: GridEntry) where
+        P: Fn(GridEntry) -> bool {
+//fn flood_fill(grid: &mut [&mut [GridEntry]], node: (usize, usize), target: GridEntry, replacement: GridEntry) {
 
-    if target == GridEntry::Infinity || target != grid[node.0][node.1] {
+    if !predicate(grid[node.0][node.1]) {
         return;
     }
 
@@ -120,33 +132,34 @@ fn flood_fill(grid: &mut [&mut [GridEntry]], node: (usize, usize), target: GridE
 
         if 1 <= n.0 {
             let west = (n.0 - 1, n.1);
-            check_paint_queue_node(grid, west, target, replacement, &mut node_q)
+            check_paint_queue_node(grid, west, &predicate, replacement, &mut node_q)
         }
 
         if n.0 < (grid.len() - 1) {
             let east = (n.0 + 1, n.1);
-            check_paint_queue_node(grid, east, target, replacement, &mut node_q)
+            check_paint_queue_node(grid, east, &predicate, replacement, &mut node_q)
         }
 
         if 1 <= n.1 {
             let north = (n.0, n.1 - 1);
-            check_paint_queue_node(grid, north, target, replacement, &mut node_q)
+            check_paint_queue_node(grid, north, &predicate, replacement, &mut node_q)
         }
 
         if n.1 < (grid[n.0].len() - 1) {
             let south = (n.0, n.1 + 1);
-            check_paint_queue_node(grid, south, target, replacement, &mut node_q)
+            check_paint_queue_node(grid, south, &predicate, replacement, &mut node_q)
         }
     }
 }
 
-fn check_paint_queue_node(
+fn check_paint_queue_node<P>(
     grid: &mut [&mut [GridEntry]],
     candidate: (usize, usize),
-    target: GridEntry, replacement: GridEntry,
-    node_q: &mut VecDeque<(usize, usize)>) -> () {
+    predicate: &P, replacement: GridEntry,
+    node_q: &mut VecDeque<(usize, usize)>) where
+        P: Fn(GridEntry) -> bool {
 
-    if grid[candidate.0][candidate.1] == target {
+    if predicate(grid[candidate.0][candidate.1]) {
         grid[candidate.0][candidate.1] = replacement;
         node_q.push_back(candidate);
     }
